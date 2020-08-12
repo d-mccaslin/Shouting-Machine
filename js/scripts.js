0497cb33271f69642c3a8cd0a1f095ca4b4824e3
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-12 12:09:45 -0700

    add functionality and html for confirmation message
    CO-AUTHORED by: Jillian Gibson jillian.l.gibson@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+  // Form handler
   $("#formOne").submit(function() {
     const phraseInput = $("input#phrase").val();
-    console.log(phraseInput);
+    const phraseUpper = phraseInput.toUpperCase();
+    // console.log(phraseUpper);
+    // alert(phraseUpper);
+
+    $(".phrase").append(phraseInput);
+    $("#input-form").hide();
+    $("#confirm").show();
 
     event.preventDefault();
   });
+  // Confirm button handlers
 });
