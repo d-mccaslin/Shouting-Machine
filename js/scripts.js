f8a7c958387458809ed3ddcb7e0454d88cf6fcd6
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-12 13:40:19 -0700

    add html and jquery for message confirmation feature
    CO-AUTHORED by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
+  let phraseUpper;
   // Form handler
   $("#formOne").submit(function() {
     const phraseInput = $("input#phrase").val();
-    const phraseUpper = phraseInput.toUpperCase();
+    phraseUpper = phraseInput.toUpperCase();
     // console.log(phraseUpper);
     // alert(phraseUpper);
 
     $(".phrase").append(phraseInput);
     $("#input-form").hide();
     $("#confirm").show();
-
+    
     event.preventDefault();
   });
-  // Confirm button handlers
+  
+  // Confirm button handler
+  $("button#yes").click(function() {
+    alert(phraseUpper);
+    $("#input-form").show();
+    $("#confirm").hide();
+  });
+  // Reject button handler
+  $("button#no").click(function() {
+    $("#input-form").show();
+    $("#confirm").hide();
+  });
 });
